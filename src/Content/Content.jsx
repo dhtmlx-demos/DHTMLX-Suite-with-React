f1d6f38d6fe0f72dccefe05d7704e9644b472104
dhtmlx-demos <155955527+dhtmlx-demos@users.noreply.github.com>
2024-02-28 08:09:54 +0300

    Update Content.jsx

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -4,7 +4,7 @@ import "./Content.css";
 
 const Content = () => {
     return (
-        <div className="container wrapper">
+        <div className="container wrapper-main">
             <LeftContent />
             <RightContent />
         </div>
@@ -12,19 +12,3 @@ const Content = () => {
 };
 
 export default Content;
-// import React from "react";
-
-// const LeftContent = () => {
-//     return <div className="container">Left Content</div>;
-// };
-
-// export default LeftContent;
-
-// // FILEPATH: /D:/IT/work/suite-react/src/Content/RightPanel/RightContent.jsx
-// import React from "react";
-
-// const RightContent = () => {
-//     return <div className="container">Right Content</div>;
-// };
-
-// export default RightContent;
